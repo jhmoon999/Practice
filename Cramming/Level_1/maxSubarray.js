fda804877aeb15228627057ce908891c76c75e34
Jason <huang.jason999@gmail.com>
2020-09-07 17:52:07 -0400

    Added some problems to CramScore Level 1

--- Cramming/Level_1/maxSubarray.js
+++ Cramming/Level_1/maxSubarray.js
@@ -16,6 +16,26 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+
+// Kadane’s Algorithm:
+
+// Initialize:
+//     max_so_far = 0
+//     max_ending_here = 0
+
+// Loop for each element of the array
+//   (a) max_ending_here = max_ending_here + a[i]
+//   (b) if(max_so_far < max_ending_here)
+//             max_so_far = max_ending_here
+//   (c) if(max_ending_here < 0)
+//             max_ending_here = 0
+// return max_so_far
+
+// Explanation:
+// Simple idea of the Kadane’s algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive sum compare it with max_so_far and update max_so_far if it is greater than max_so_far
+
+// Time: O(n), Space: O(1)
+
     let maxSum = -Infinity, currentSum = 0;
     
     for (let i = 0; i < nums.length; i++) {
