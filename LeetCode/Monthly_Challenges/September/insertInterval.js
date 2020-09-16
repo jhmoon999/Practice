53384b35fe8f4a1bf0c75ac5bb3ef161031c19be
Jason <huang.jason999@gmail.com>
2020-09-15 21:58:24 -0400

    Added September Week 2 to LeetCode Monthly Challenge

--- LeetCode/Monthly_Challenges/September/insertInterval.js
+++ LeetCode/Monthly_Challenges/September/insertInterval.js
@@ -19,17 +19,28 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
+// First, all intervals that have end times before newInterval's start time can
+// be inserted into answer array without alteration. Now we are left only with
+// intervals that have end times after newInterval's start time. Add in the 
+// condition that the start time has to be before newInterval's end time, and
+// we have found all intervals that need to be merged into newInterval. Finally
+// we are left only with intervals that have start times after newInterval's
+// end time, and they can be inserted into answer array without alteration.
+// Time: O(n), Space: O(1)
     let ans = [], i = 0;
+    // intervals before newInterval
     while (i < intervals.length && intervals[i][1] < newInterval[0]) {
         ans.push(intervals[i]);
         i += 1;
     }
+    // intervals to be merged into newInterval
     while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
         newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
         newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
         i += 1;
     }
     ans.push(newInterval);
+    // intervals after newInterval
     while (i < intervals.length) {
         ans.push(intervals[i]);
         i += 1;
