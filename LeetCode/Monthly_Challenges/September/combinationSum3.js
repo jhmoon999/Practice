9ef824c9157344e7a3206ad22014910735de87c6
Jason <huang.jason999@gmail.com>
2020-09-17 23:50:36 -0400

    Added September Week 2 to LeetCode Monthly Challenge

--- LeetCode/Monthly_Challenges/September/combinationSum3.js
+++ LeetCode/Monthly_Challenges/September/combinationSum3.js
@@ -20,18 +20,35 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
+// Brute force - DFS recursively create every valid combination 
+// Time: O(9!*k / (9-k)!) - there are 9! total combinations, but divide that 
+// by (9-k)! for combinations with all unique digits. Every combination takes
+// O(k) time to process validity.
+// Space: O(k) due to recursive stack
+
+    // ans array holds all valid combinations
+    // combo array holds current valid combination
     const ans = [], combo = [];
     
     const findComboSum = function(currSum) {
+        // once penultimate digit is reached, the last digit is simply 
+        // n - sum of all current digits in combo
         if (combo.length === k - 1) {
             let lastNum = n - currSum;
+            // if lastNum is more than 9, it is not a digit
+            // if lastNum is less than most recently added digit in combo,
+            // then combo array is not in order and combination is invalid
             if (lastNum <= 9 && lastNum > combo[combo.length - 1]) {
+                // if conditions reached, add combo into ans array
                 combo.push(lastNum);
                 ans.push(combo.slice());
                 combo.pop();
             }
             return;
         }
+        //* at the start when combo is empty, j = 1
+        //* after that, j is the last digit in combo (because we know that 
+        //* next digit will be larger due to combo being in order)
         for (let j = combo[combo.length - 1] + 1 || 1; j <= 9; j++) {
             if (currSum + j <= n) {
                 combo.push(j);
